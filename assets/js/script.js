31474d394ddba69c073ecddce4af308df8dc7575
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-01-06 10:26:24 +0000

    Added comments in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,25 @@ var plannerHours = [9, 10, 11, 12, 13, 14, 15, 16, 21];
 var currentHour = dayjs();
 // Display current day in Header (date format Friday, January 05)
 $("#currentDay").text(dayjs().format("dddd[,] MMMM DD"));
-// Create time block
+// Create time blocks depending on plannerHours length
 for (var i = 0; i < plannerHours.length; i++) {
+  // Create main time block div with classes
   var timeBlock = $("<div>");
   timeBlock.addClass("row time-block");
-  // Create hour div
+  // Create hour div with classes
   var hourDiv = $("<div>");
   hourDiv.addClass(
     "col-2 hour d-flex justify-content-center align-items-center"
   );
+  // Setting div value to time (format ex. 10 AM), based on plannerHours array (array values are from 0 -24 to represent 24 hours)
   var time = dayjs().hour(plannerHours[i]).minute(0);
   var timeDisplay = time.format("h A");
   hourDiv.text(timeDisplay);
-  // Create Textarea
+  // Create Textarea with classes and dataset
   var textArea = $("<textarea>");
   textArea.attr("data-order", i + 1);
   textArea.addClass("col-8 description");
-  // Create Button
+  // Create Button with classes and dataset
   var saveBtn = $("<button>");
   saveBtn.attr("data-order", i + 1);
   saveBtn.addClass("col-2 saveBtn");
@@ -35,8 +37,11 @@ for (var i = 0; i < plannerHours.length; i++) {
   timeBlock.append(hourDiv);
   timeBlock.append(textArea);
   timeBlock.append(saveBtn);
-  var differenceInMinutess = currentHour.diff(time, "minutes");
 
+  // Subtraction of current time and planners time (depending on time block).  Result is in minutes
+  var differenceInMinutess = currentHour.diff(time, "minutes");
+  // Condition to check if subtraction result is negative, inbetween of 60 or over 60
+  // Based on result add relevent class to textArea
   if (differenceInMinutess < 0) {
     textArea.addClass("future");
   } else if (differenceInMinutess <= 60 && differenceInMinutess >= 0) {
@@ -45,23 +50,28 @@ for (var i = 0; i < plannerHours.length; i++) {
     textArea.addClass("past");
   }
 
-  // Target textarea depening on data-order value
+  // Get text area value from local storage
   var savedTextAreaValue = localStorage.getItem("Text Area Input" + (i + 1));
+  // Display value in text area
   textArea.val(savedTextAreaValue);
 }
-// Click event on button click
+// Click event on button click (button inside div with container class .saveBtn)
 container.on("click", ".saveBtn", function () {
+  // Getting dataset order value
   var buttonOrder = $(this).data("order");
+  // Getting dataset order value of text area
   var textAreaOrder = $(this).prev().data("order");
+  // Targeting value of a text area with specific dataset order value
   var textAreaOrderValue = $(
     ".description[data-order=" + textAreaOrder + "]"
   ).val();
 
   // Update local storage with the corresponding textarea value
   localStorage.setItem("Text Area Input" + textAreaOrder, textAreaOrderValue);
+  // Getting updated value of text area from local storage
   var updatedTextAreaValue = localStorage.getItem(
     "Text Area Input" + textAreaOrder
   );
-
+  // Display updated value inside text area from local storage
   $(this).prev().val(updatedTextAreaValue);
 });
